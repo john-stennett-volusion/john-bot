d7e26947de41a3ee5e061371ff4499401da97b77
john-stennett-volusion <john_stennett@volusion.com>
2016-05-03 10:53:01 -0500

    - Converted robot.js to ES2015. - Adding compiled copies. - Renaming old robot.js file.

--- robot-compiled.js
+++ robot-compiled.js
@@ -1,181 +1,168 @@
-var DOMParser = require('xmldom').DOMParser;
-var Botkit = require('botkit');
-var http = require('http');
-var request = require('request');
-var Chance = require('chance');
-var Secret = require('./secret.js');
-var slackToken;
+'use strict';
 
+let DOMParser = require('xmldom').DOMParser;
+let Botkit = require('botkit');
+let http = require('http');
+let request = require('request');
+let Chance = require('chance');
+let Secret = require('./secret.js');
+
+let chance = new Chance();
+let weatherToken = 'bfbb9867aa3f80a7';
+let slackToken;
+
+// Define the correct Slack Token based on environment.
 if (process.env.SLACK_TOKEN == null) {
 	slackToken = Secret.slackToken;
 } else {
 	slackToken = process.env.SLACK_TOKEN;
 }
 
-var weatherToken = 'bfbb9867aa3f80a7';
-var obamaToken = '60c6a10d525a7b198b73245bc2591b42';
-var chance = new Chance();
-
-var controller = Botkit.slackbot({
+let controller = Botkit.slackbot({
 	debug: false,
 	retry: false
 	//include "log: false" to disable logging
 	//or a "logLevel" integer from 0 to 7 to adjust logging verbosity
 });
 
-// connect the bot to a stream of messages
-var bot = controller.spawn({
+// Connect the bot to a stream of messages.
+let bot = controller.spawn({
 	token: slackToken
 });
 
-bot.startRTM(function (err) {
+// Start the bot.
+bot.startRTM(err => {
 	if (err) {
 		throw new Error('Could not connect to Slack!');
 	}
 });
 
-// give the bot something to listen for.
-controller.hears('hello', ['direct_message', 'direct_mention', 'mention'], function (bot, message) {
+// Give the bot something to listen for.
+controller.hears('hello', ['direct_message', 'direct_mention', 'mention'], (bot, message) => {
 	bot.reply(message, 'Hello yourself.');
 });
 
-controller.hears('weather', ['direct_message', 'direct_mention', 'mention'], function (bot, message) {
+controller.hears('help', ['direct_message', 'direct_mention', 'mention'], (bot, message) => {
+	let slackMessage = message.text.toLowerCase();
 
-	var txt = message.text;
-	txt = txt.toLowerCase().replace('weather', '');
+	if (slackMessage == 'help') {
+		let helpMessage = '';
+		helpMessage += `\`\`\`hello               - Returns "Hello yourself".\n`;
+		helpMessage += `help                - Returns a list of available commands.\n`;
+		helpMessage += `weather CITY, STATE - Include CITY, STATE & you'll get the weather.\n`;
+		helpMessage += `google SEARCH_TERM  - Include a SEARCH_TERM & you'll get a link to Google.\n`;
+		helpMessage += `lorem               - Returns a random paragraph of Lorem Ipsum.\n`;
+		helpMessage += `xkcd                - Returns the most recent XKCD comic.\n`;
+		helpMessage += `xkcd random         - Returns a random XKCD comic.\n`;
+		helpMessage += `xkcd COMIC_NUMBER   - Returns the specified XKCD comic #.\`\`\``;
 
-	var city = txt.split(',')[0].trim().replace(' ', '_');
-	var state = txt.split(',')[1].trim();
+		bot.reply(message, helpMessage);
+	}
+});
 
-	console.log(city + ', ' + state);
+controller.hears('weather', ['direct_message', 'direct_mention', 'mention'], (bot, message) => {
+	let weatherMessage = message.text.toLowerCase();
+	weatherMessage = weatherMessage.replace('weather', '').split(',');
 
-	var url = '/api/' + weatherToken + '/forecast/q/state/city.json';
-	url = url.replace('state', state);
-	url = url.replace('city', city);
+	let city = weatherMessage[0].trim().replace(' ', '_');
+	let state = weatherMessage[1].trim();
+
+	let weatherUri = `/api/${ weatherToken }/forecast/q/${ state }/${ city }.json`;
 
 	http.get({
 		host: 'api.wunderground.com',
-		path: url
-	}, function (response) {
-		var body = '';
-		response.on('data', function (d) {
-			body += d;
+		path: weatherUri
+	}, response => {
+		let weatherBody = '';
+
+		response.on('data', d => {
+			weatherBody += d;
 		});
-		response.on('end', function () {
-			var data = JSON.parse(body);
-			var days = data.forecast.simpleforecast.forecastday;
+		response.on('end', () => {
+			let weatherData = JSON.parse(weatherBody);
+			let weatherDays = weatherData.forecast.simpleforecast.forecastday;
 
-			console.log(days);
+			for (let i = 0; i < weatherDays.length; i++) {
+				let weatherDay = `*${ weatherDays[i].date.weekday }*`;
+				let weatherHigh = `*High*: ${ weatherDays[i].high.fahrenheit }`;
+				let weatherLow = `*Low*: ${ weatherDays[i].low.fahrenheit }`;
+				let weatherConditions = `*Conditions*: ${ weatherDays[i].conditions }`;
+				let weatherMessage = `${ weatherDay } - ${ weatherHigh } - ${ weatherLow } - ${ weatherConditions }`;
 
-			for (var i = 0; i < days.length; i++) {
-				bot.reply(message, days[i].date.weekday + ' high: ' + days[i].high.fahrenheit + ' low: ' + days[i].low.fahrenheit + ' conditions: ' + days[i].conditions);
-				//bot.reply(message, days[i].icon_url);
+				bot.reply(message, weatherMessage);
 			}
 		});
 	});
 });
 
-controller.hears('google', ['direct_message', 'direct_mention', 'mention'], function (bot, message) {
+controller.hears('google', ['direct_message', 'direct_mention', 'mention'], (bot, message) => {
+	let googleMessage = message.text.toLowerCase().replace('google', '');
+	googleMessage = googleMessage.slice(1, googleMessage.length).replace(/ /g, '+');
 
-	var txt = message.text;
-	txt = txt.toLowerCase().replace('google', '');
-	txt = txt.slice(1, txt.length);
-	txt = txt.toLowerCase().replace(/ /g, '+');
+	let googleUrl = `https://www.google.com/#q=${ googleMessage }`;
 
-	var url = 'https://www.google.com/#q=' + txt;
-
-	bot.reply(message, url);
+	bot.reply(message, googleUrl);
 });
 
-controller.hears('pugbomb', ['direct_message', 'direct_mention', 'mention'], function (bot, message) {
-	bot.reply(message, 'Why do you want to look at pugs? Why not Shar-Peis?');
-});
+controller.hears('xkcd', ['direct_message', 'direct_mention', 'mention'], (bot, message) => {
+	let xkcdMessage = message.text.toLowerCase().replace('xkcd', '');
+	xkcdMessage = xkcdMessage.slice(1, xkcdMessage.length);
+	let comicUrl;
 
-controller.hears('talkobamatome', ['direct_message', 'direct_mention', 'mention'], function (bot, message) {
+	let sendMessage = url => {
 
-	var txt = message.text;
-	txt = txt.toLowerCase().replace('talkobamatome', '');
-	txt = txt.slice(1, txt.length);
-
-	bot.reply(message, 'Processing your request, please hold.');
-
-	request.post({
-		url: 'http://talkobamato.me/synthesize.py',
-		form: {
-			input_text: txt
+		if (xkcdMessage.length == 0) {
+			comicUrl = 'http://xkcd.com/info.0.json';
+		} else if (typeof xkcdMessage == "number") {
+			comicUrl = `http://xkcd.com/${ xkcdMessage }/info.0.json`;
+		} else {
+			comicUrl = url;
 		}
-	}, function (err, httpResponse, body) {
-		var data = body;
-		var parser = new DOMParser();
-		var htmlDoc = parser.parseFromString(data, "text/html");
-		var video = htmlDoc.getElementsByTagName('a');
-		var address = video[0].getAttribute('href');
-		var videoKey = address.split('=')[1];
-		var videoLink = 'http://talkobamato.me/synth/output/' + videoKey + '/obama.mp4';
 
-		bot.reply(message, videoLink);
-	});
-});
-
-controller.hears('lorem', ['direct_message', 'direct_mention', 'mention'], function (bot, message) {
-	var txt = message.text;
-	var words = chance.paragraph();
-
-	bot.reply(message, words);
-});
-
-controller.hears('xkcd', ['direct_message', 'direct_mention', 'mention'], function (bot, message) {
-	var messageText = message.text;
-	var splitText = message.text;
-	splitText = splitText.toLowerCase().replace('xkcd', '');
-	splitText = splitText.slice(1, splitText.length);
-
-	var totalComicCount;
-	var randomComic;
-
-	if (messageText.toLowerCase() == 'xkcd' && messageText.length == 4) {
 		request.post({
-			url: 'http://xkcd.com/info.0.json'
-		}, function (err, httpResponse, body) {
-			var replyBody = JSON.parse(body);
-			var title = replyBody.safe_title;
-			var alt = replyBody.alt;
-			var image = replyBody.img;
+			url: comicUrl
+		}, (err, httpResponse, body) => {
+			let replyBody = JSON.parse(body);
+			let title = replyBody.safe_title;
+			let alt = replyBody.alt;
+			let image = replyBody.img;
 
 			bot.reply(message, '*' + title + '*');
 			bot.reply(message, image + '\n' + alt);
 		});
-	} else if (splitText.toLowerCase().indexOf('random') > -1) {
-		request.post({
-			url: 'http://xkcd.com/info.0.json'
-		}, function (err, httpResponse, body) {
-			var replyBody = JSON.parse(body);
-			totalComicCount = String(replyBody.num);
-			randomComic = chance.integer({ min: 1, max: totalComicCount });
+	};
 
-			request.post({
-				url: 'http://xkcd.com/' + randomComic + '/info.0.json'
-			}, function (err, httpResponse, body) {
-				var replyBody = JSON.parse(body);
-				var title = replyBody.safe_title;
-				var alt = replyBody.alt;
-				var image = replyBody.img;
+	request.post({
+		url: 'http://xkcd.com/info.0.json'
+	}, (err, httpResponse, body) => {
+		let replyBody = JSON.parse(body);
+		let totalComicCount = String(replyBody.num);
 
-				bot.reply(message, '*' + title + '*');
-				bot.reply(message, image + '\n' + '*' + alt + '*');
-			});
+		let randomComicNumber = chance.integer({
+			min: 1,
+			max: Number(totalComicCount)
 		});
-	} else if (typeof Number(splitText) == "number") {
-		request.post({
-			url: 'http://xkcd.com/' + splitText + '/info.0.json'
-		}, function (err, httpResponse, body) {
-			var replyBody = JSON.parse(body);
-			var title = replyBody.safe_title;
-			var alt = replyBody.alt;
-			var image = replyBody.img;
 
-			bot.reply(message, '*' + title + '*');
-			bot.reply(message, image + '\n' + '*' + alt + '*');
+		let randomUrl = `http://xkcd.com/${ randomComicNumber }/info.0.json`;
+
+		sendMessage(randomUrl);
+	});
+});
+
+controller.hears('ch', ['direct_message', 'direct_mention', 'mention'], (bot, message) => {
+	let chMessage = message.text.toLowerCase().replace('ch', '');
+
+	if (chMessage.length == 0) {
+		request.post({
+			url: 'http://explosm.net/comics/latest/'
+		}, (err, httpResponse, body) => {
+			let data = body;
+			let parse = new DOMParser();
+			let htmlDoc = parse.parseFromString(data, "text/html");
+			let image = htmlDoc.getElementById('main-comic');
+			let address = image.getAttribute('src');
+
+			bot.reply(message, address);
 		});
 	}
 });
---
+++ robot-old.js
@@ -0,0 +1,187 @@
+var DOMParser = require('xmldom').DOMParser;
+var Botkit    = require('botkit');
+var http      = require('http');
+var request   = require('request');
+var Chance    = require('chance');
+var Secret    = require('./secret.js');
+var slackToken;
+
+if (process.env.SLACK_TOKEN == null) {
+	slackToken = Secret.slackToken;
+} else {
+	slackToken = process.env.SLACK_TOKEN;
+}
+
+var weatherToken = 'bfbb9867aa3f80a7';
+var obamaToken   = '60c6a10d525a7b198b73245bc2591b42';
+var chance       = new Chance();
+
+var controller = Botkit.slackbot({
+	debug: false,
+	retry: false
+	//include "log: false" to disable logging
+	//or a "logLevel" integer from 0 to 7 to adjust logging verbosity
+});
+
+// connect the bot to a stream of messages
+var bot = controller.spawn({
+	token: slackToken
+});
+
+bot.startRTM(function(err) {
+	if (err) {
+		throw new Error('Could not connect to Slack!');
+	}
+});
+
+// give the bot something to listen for.
+controller.hears('hello', ['direct_message', 'direct_mention', 'mention'], function(bot, message) {
+	bot.reply(message, 'Hello yourself.');
+});
+
+controller.hears('weather', ['direct_message', 'direct_mention', 'mention'], function(bot, message) {
+
+	var txt = message.text;
+	txt = txt.toLowerCase().replace('weather', '');
+
+	var city = txt.split(',')[0].trim().replace(' ', '_');
+	var state = txt.split(',')[1].trim();
+
+	console.log(city + ', ' + state);
+
+	var url = '/api/' + weatherToken + '/forecast/q/state/city.json';
+	url = url.replace('state', state);
+	url = url.replace('city', city);
+
+	http.get({
+		host: 'api.wunderground.com',
+		path: url
+	}, function(response) {
+		var body = '';
+		response.on('data', function(d) {
+			body += d;
+		});
+		response.on('end', function() {
+			var data = JSON.parse(body);
+			var days = data.forecast.simpleforecast.forecastday;
+
+			console.log(days);
+
+			for (var i = 0; i < days.length; i++) {
+				bot.reply(message, days[i].date.weekday +
+					' high: ' + days[i].high.fahrenheit +
+					' low: ' + days[i].low.fahrenheit +
+					' conditions: ' + days[i].conditions);
+				//bot.reply(message, days[i].icon_url);
+			}
+		});
+	});
+
+});
+
+controller.hears('google', ['direct_message', 'direct_mention', 'mention'], function(bot, message) {
+
+	var txt = message.text;
+	txt = txt.toLowerCase().replace('google', '');
+	txt = txt.slice(1, txt.length);
+	txt = txt.toLowerCase().replace(/ /g, '+');
+
+	var url = 'https://www.google.com/#q=' + txt;
+
+	bot.reply(message, url);
+
+});
+
+controller.hears('pugbomb', ['direct_message', 'direct_mention', 'mention'], function(bot, message) {
+	bot.reply(message, 'Why do you want to look at pugs? Why not Shar-Peis?');
+});
+
+controller.hears('talkobamatome', ['direct_message', 'direct_mention', 'mention'], function(bot, message) {
+
+	var txt = message.text;
+	txt = txt.toLowerCase().replace('talkobamatome', '');
+	txt = txt.slice(1, txt.length);
+
+	bot.reply(message, 'Processing your request, please hold.');
+
+	request.post({
+		url: 'http://talkobamato.me/synthesize.py',
+		form: {
+			input_text: txt
+		}
+	}, function(err, httpResponse, body) {
+		var data      = body;
+		var parser    = new DOMParser();
+		var htmlDoc   = parser.parseFromString(data, "text/html");
+		var video     = htmlDoc.getElementsByTagName('a');
+		var address   = video[0].getAttribute('href');
+		var videoKey  = address.split('=')[1];
+		var videoLink = 'http://talkobamato.me/synth/output/' + videoKey + '/obama.mp4';
+
+		bot.reply(message, videoLink);
+	});
+
+});
+
+controller.hears('lorem', ['direct_message', 'direct_mention', 'mention'], function(bot, message) {
+	var txt = message.text;
+	var words = chance.paragraph();
+
+	bot.reply(message, words);
+});
+
+controller.hears('xkcd', ['direct_message', 'direct_mention', 'mention'], function(bot, message) {
+	var messageText = message.text;
+	var splitText = message.text;
+	splitText = splitText.toLowerCase().replace('xkcd', '');
+	splitText = splitText.slice(1, splitText.length);
+
+	var totalComicCount;
+	var randomComic;
+
+	if (messageText.toLowerCase() == 'xkcd' && messageText.length == 4) {
+		request.post({
+			url: 'http://xkcd.com/info.0.json'
+		}, function(err, httpResponse, body) {
+			var replyBody = JSON.parse(body);
+			var title = replyBody.safe_title;
+			var alt = replyBody.alt;
+			var image = replyBody.img;
+
+			bot.reply(message, ('*' + title + '*'));
+			bot.reply(message, (image + '\n' + alt));
+		});
+	} else if (splitText.toLowerCase().indexOf('random') > -1) {
+		request.post({
+			url: 'http://xkcd.com/info.0.json'
+		}, function(err, httpResponse, body) {
+			var replyBody = JSON.parse(body);
+			totalComicCount = String(replyBody.num);
+			randomComic = chance.integer({ min: 1, max: totalComicCount });
+
+			request.post({
+				url: 'http://xkcd.com/' + randomComic + '/info.0.json'
+			}, function(err, httpResponse, body) {
+				var replyBody = JSON.parse(body);
+				var title = replyBody.safe_title;
+				var alt = replyBody.alt;
+				var image = replyBody.img;
+
+				bot.reply(message, ('*' + title + '*'));
+				bot.reply(message, (image + '\n' + '*' + alt + '*'));
+			});
+		});
+	} else if (typeof Number(splitText) == "number") {
+		request.post({
+			url: 'http://xkcd.com/' + splitText + '/info.0.json'
+		}, function(err, httpResponse, body) {
+			var replyBody = JSON.parse(body);
+			var title = replyBody.safe_title;
+			var alt = replyBody.alt;
+			var image = replyBody.img;
+			
+			bot.reply(message, ('*' + title + '*'));
+			bot.reply(message, (image + '\n' + '*' + alt + '*'));
+		});
+	}
+});
